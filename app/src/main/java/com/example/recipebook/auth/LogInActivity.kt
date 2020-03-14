3d3e81230268a81fc847eace975587fa18d5a448
Florence-Njeri <flonsie17@gmail.com>
2020-03-14 11:16:03 +0300

    This is a new commit for what I originally planned to be amended

---
+++ app/src/main/java/com/example/recipebook/auth/LogInActivity.kt
@@ -0,0 +1,12 @@
+package com.example.recipebook.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+    }
+}
