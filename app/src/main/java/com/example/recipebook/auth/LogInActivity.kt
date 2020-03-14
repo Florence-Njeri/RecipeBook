9107ea472c6a06ce1900bb0a666bbd86475267b3
Florence-Njeri <flonsie17@gmail.com>
2020-03-14 20:03:37 +0300

    design log in page

--- app/src/main/java/com/example/recipebook/auth/LogInActivity.kt
+++ app/src/main/java/com/example/recipebook/auth/LogInActivity.kt
@@ -2,6 +2,7 @@ package com.example.recipebook.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.recipebook.R
 
 class LogInActivity : AppCompatActivity() {
 
