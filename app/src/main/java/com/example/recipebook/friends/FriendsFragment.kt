a6ce001c6537f436cfd681726bc5cfa46b1a6b83
Florence-Njeri <flonsie17@gmail.com>
2020-03-20 19:37:43 +0300

    design friends fragment layout

---
+++ app/src/main/java/com/example/recipebook/friends/FriendsFragment.kt
@@ -0,0 +1,32 @@
+package com.example.recipebook.friends
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.example.recipebook.R
+
+class FriendsFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = FriendsFragment()
+    }
+
+    private lateinit var viewModel: FriendsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.friends_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(FriendsViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/recipebook/friends/FriendsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.recipebook.friends
+
+import androidx.lifecycle.ViewModel
+
+class FriendsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
