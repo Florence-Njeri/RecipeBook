a73f8bf64449a508f1e853a7bf2431b8b92cd0c9
Florence-Njeri <flonsie17@gmail.com>
2020-03-21 13:50:56 +0300

    add back navigation to the fragments

--- app/src/main/java/com/example/recipebook/MainActivity.kt
+++ app/src/main/java/com/example/recipebook/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.recipebook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,12 +14,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-
-
-        //Find NavController
+        setSupportActionBar(binding.toolbar)        //Find NavController
         val navController = this.findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this,navController)
 
-
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()
     }
 }
 
